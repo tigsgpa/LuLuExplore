3adfd68ffad0a60bf176819d102925294e2058e5
tigsgpa <tigsgpa@gmail.com>
2024-11-24 01:13:54 +0530

    Update Exploreby.jsx

--- src/components/Exploreby.jsx
+++ src/components/Exploreby.jsx
@@ -12,7 +12,7 @@ const Exploreby = () => {
   }, []);
 
   useEffect(() => {
-    document.body.style.zoom = "90%"; // Set zoom level to 80%
+    document.body.style.zoom = "80%"; // Set zoom level to 80%
     return () => {
       document.body.style.zoom = "100%"; // Reset on component unmount
     };
