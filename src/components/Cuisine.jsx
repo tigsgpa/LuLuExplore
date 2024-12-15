5aefc7e5ae89824f90e96f9184673d0d757cfac3
tigsgpa <tigsgpa@gmail.com>
2024-12-15 11:23:28 +0530

    Update Cuisine.jsx

--- src/components/Cuisine.jsx
+++ src/components/Cuisine.jsx
@@ -13,7 +13,7 @@ const FoodMenuAndCuisine = () => {
   }, []);
 
   useEffect(() => {
-    document.body.style.zoom = "90%"; // Set zoom level to 80%
+    document.body.style.zoom = "80%"; // Set zoom level to 80%
     return () => {
       document.body.style.zoom = "100%"; // Reset on component unmount
     };
